a115bfa9d98a94af213d51af331bb8cfc87d3479
Liliana Fernandes <liliana@yeti.local>
2024-12-23 00:11:01 +0000

    add day 13

--- day13/part1.js
+++ day13/part1.js
@@ -1,202 +1,93 @@
 import { readLines } from "https://deno.land/std@0.224.0/io/mod.ts";
 
-const gardens = [];
+const defaultMachineValues = {
+  A: {
+    x: 0,
+    y: 0,
+  },
+  B: {
+    x: 0,
+    y: 0,
+  },
+  Prize: {
+    x: 0,
+    y: 0,
+  },
+};
+const machines = [];
+let currentMachine = structuredClone(defaultMachineValues);
 
 for await (const line of readLines(Deno.stdin)) {
-  gardens.push(line.split(""));
+  if (line === "") {
+    machines.push(currentMachine);
+    currentMachine = structuredClone(defaultMachineValues);
+  } else if (line.startsWith("Button")) {
+    const [buttonName, values] = line.split(":");
+    const [, name] = buttonName.split(" ");
+    const [xPart, yPart] = values.split(",");
+    const [, x] = xPart.split("+");
+    const [, y] = yPart.split("+");
+
+    currentMachine[name] = {
+      x: Number(x),
+      y: Number(y),
+    };
+  } else if (line.startsWith("Prize")) {
+    const [name, values] = line.split(":");
+    const [xPart, yPart] = values.split(",");
+    const [, x] = xPart.split("=");
+    const [, y] = yPart.split("=");
+
+    currentMachine[name] = {
+      x: Number(x),
+      y: Number(y),
+    };
+  }
 }
 
-const adjacentPlotsWithPositions = (rowIndex, colIndex, gardens) => {
-  const positions = [];
+// Add last machine
+machines.push(currentMachine);
 
-  // top
-  if (rowIndex > 0) {
-    const neighborRowIndex = rowIndex - 1;
-    const neighborColIndex = colIndex;
+const timesForAandB = (machine) => {
+  const timesForADenominator =
+    machine.A.y * machine.B.x - machine.A.x * machine.B.y;
+  const timesForANumerator =
+    machine.Prize.y * machine.B.x - machine.Prize.x * machine.B.y;
+  const timesForA = timesForANumerator / timesForADenominator;
 
-    const neighbor = gardens[neighborRowIndex][neighborColIndex];
+  const timesForBNumerator = machine.Prize.x - machine.A.x * timesForA;
+  const timesForBDenominator = machine.B.x;
+  const timesForB = timesForBNumerator / timesForBDenominator;
 
-    positions.push([neighbor, neighborRowIndex, neighborColIndex]);
-  }
-
-  // left
-  if (colIndex > 0) {
-    const neighborRowIndex = rowIndex;
-    const neighborColIndex = colIndex - 1;
-    const neighbor = gardens[neighborRowIndex][neighborColIndex];
-
-    positions.push([neighbor, neighborRowIndex, neighborColIndex]);
-  }
-
-  // right
-  if (colIndex < gardens[0].length - 1) {
-    const neighborRowIndex = rowIndex;
-    const neighborColIndex = colIndex + 1;
-    const neighbor = gardens[neighborRowIndex][neighborColIndex];
-
-    positions.push([neighbor, neighborRowIndex, neighborColIndex]);
-  }
-
-  // bottom
-  if (rowIndex < gardens.length - 1) {
-    const neighborRowIndex = rowIndex + 1;
-    const neighborColIndex = colIndex;
-
-    const neighbor = gardens[neighborRowIndex][neighborColIndex];
-
-    positions.push([neighbor, neighborRowIndex, neighborColIndex]);
-  }
-
-  return positions;
+  return {
+    A: timesForA,
+    B: timesForB,
+  };
 };
 
-const regionArea = (region) => {
-  return region.length;
+const numberOfTokens = (timesForAandB) => {
+  return timesForAandB.A * 3 + timesForAandB.B * 1;
 };
 
-const regionPerimeter = (region, gardens) => {
-  let perimeter = 0;
+const numberOfTokenToWinAllPrizes = (machines) => {
+  let totalTokens = 0;
 
-  region.forEach((plot) => {
-    const [plotValue, plotRow, plotCol] = plot;
-    let finalOutsideSides;
+  machines.forEach((machine) => {
+    const times = timesForAandB(machine);
+    const areTimesLessThanZero =
+      Object.values(times).filter((time) => time <= 100).length === 2;
 
-    const adjacentPlots = adjacentPlotsWithPositions(
-      plotRow,
-      plotCol,
-      gardens,
-    ).filter((plot) => plot[0] !== plotValue);
+    if (areTimesLessThanZero) {
+      const tokens = numberOfTokens(times);
+      const isPossibleToWin = Number.isInteger(tokens);
 
-    finalOutsideSides = adjacentPlots.length;
-
-    if (plotRow === 0) {
-      finalOutsideSides += 1;
+      if (isPossibleToWin) {
+        totalTokens += tokens;
+      }
     }
-
-    if (plotCol === 0) {
-      finalOutsideSides += 1;
-    }
-
-    if (plotRow === gardens.length - 1) {
-      finalOutsideSides += 1;
-    }
-
-    if (plotCol === gardens[0].length - 1) {
-      finalOutsideSides += 1;
-    }
-
-    perimeter += finalOutsideSides;
   });
 
-  return perimeter;
+  return totalTokens;
 };
 
-const findRegion = (initialRow, initialCol, gardens) => {
-  const differentGardenNameToExplore = [];
-  const sameGardenNameToExplore = [[initialRow, initialCol]];
-  const currentGardens = [];
-};
-
-const allRegions = (gardens) => {
-  const visitedPositionsAsString = [];
-  let currentRegion = [[gardens[0][0], 0, 0]];
-  let currentGarden = [];
-  let plotsToExploreNext = [];
-  const regions = [];
-
-  while (currentRegion.length > 0) {
-    const [value, rowIndex, colIndex] = currentRegion.pop();
-    const posToString = `${rowIndex}|${colIndex}`;
-
-    if (visitedPositionsAsString.includes(posToString)) {
-      const nextPlot = plotsToExploreNext.pop();
-
-      if (nextPlot) {
-        currentRegion = [nextPlot];
-      }
-
-      continue;
-    }
-
-    const adjacentPlots = adjacentPlotsWithPositions(
-      rowIndex,
-      colIndex,
-      gardens,
-    ).filter(
-      (plot) => !visitedPositionsAsString.includes(`${plot[1]}|${plot[2]}`),
-    );
-
-    const zeroAdjSameRegionPlots =
-      adjacentPlots.filter((plot) => plot[0] === value).length === 0;
-
-    if (currentGarden.length > 0 && (currentGarden[0] || [])[0] !== value) {
-      plotsToExploreNext = [[value, rowIndex, colIndex], ...plotsToExploreNext];
-      continue;
-    }
-
-    if (zeroAdjSameRegionPlots) {
-      if (
-        currentRegion.length > 0 &&
-        currentRegion.filter((garden) => garden[0] === value).length ===
-          currentRegion.length
-      ) {
-        continue;
-      } else {
-        const nextPlot = plotsToExploreNext.pop();
-
-        regions.push(
-          new Set(currentGarden.map((plot) => `${plot[1]}|${plot[2]}`)),
-        );
-        currentGarden = [];
-
-        if (nextPlot) {
-          currentRegion = [nextPlot];
-        } else {
-          currentRegion = [];
-        }
-        break;
-      }
-    } else {
-      adjacentPlots.forEach((plot) => {
-        const adjPosToString = `${plot[1]}|${plot[2]}`;
-        if (!visitedPositionsAsString.includes(adjPosToString)) {
-          const adjPlotValue = gardens[plot[1]][plot[2]];
-
-          if (adjPlotValue === value) {
-            currentRegion = [...currentRegion, plot];
-          } else {
-            plotsToExploreNext = [plot, ...plotsToExploreNext];
-          }
-        }
-      });
-
-      if (value === currentRegion[0][0]) {
-        currentGarden.push([value, rowIndex, colIndex]);
-      }
-    }
-
-    visitedPositionsAsString.push(`${rowIndex}|${colIndex}`);
-  }
-
-  return regions;
-};
-
-const totalPrice = (gardens) => {
-  const regions = allRegions(gardens);
-  let total = 0;
-
-  regions.forEach((region) => {
-    const parsedRegion = Array.from(region).map((positionString) => {
-      const positions = positionString.split("|").map((value) => Number(value));
-
-      return [gardens[positions[0]][positions[1]], ...positions];
-    });
-    const area = regionArea(parsedRegion);
-    const perimeter = regionPerimeter(parsedRegion, gardens);
-    total += area * perimeter;
-  });
-
-  return total;
-};
-
-console.log(totalPrice(gardens));
+console.log(numberOfTokenToWinAllPrizes(machines));
--- day13/part2.js
+++ day13/part2.js
@@ -1,244 +1,88 @@
 import { readLines } from "https://deno.land/std@0.224.0/io/mod.ts";
 
-const gardens = [];
+const defaultMachineValues = {
+  A: {
+    x: 0,
+    y: 0,
+  },
+  B: {
+    x: 0,
+    y: 0,
+  },
+  Prize: {
+    x: 0,
+    y: 0,
+  },
+};
+const machines = [];
+let currentMachine = structuredClone(defaultMachineValues);
 
 for await (const line of readLines(Deno.stdin)) {
-  gardens.push(line.split(""));
+  if (line === "") {
+    machines.push(currentMachine);
+    currentMachine = structuredClone(defaultMachineValues);
+  } else if (line.startsWith("Button")) {
+    const [buttonName, values] = line.split(":");
+    const [, name] = buttonName.split(" ");
+    const [xPart, yPart] = values.split(",");
+    const [, x] = xPart.split("+");
+    const [, y] = yPart.split("+");
+
+    currentMachine[name] = {
+      x: Number(x),
+      y: Number(y),
+    };
+  } else if (line.startsWith("Prize")) {
+    const [name, values] = line.split(":");
+    const [xPart, yPart] = values.split(",");
+    const [, x] = xPart.split("=");
+    const [, y] = yPart.split("=");
+
+    currentMachine[name] = {
+      x: Number(x) + 10000000000000,
+      y: Number(y) + 10000000000000,
+    };
+  }
 }
 
-const adjacentPlotsWithPositions = (rowIndex, colIndex, gardens) => {
-  const positions = [];
+// Add last machine
+machines.push(currentMachine);
 
-  // top
-  if (rowIndex > 0) {
-    const neighborRowIndex = rowIndex - 1;
-    const neighborColIndex = colIndex;
+const timesForAandB = (machine) => {
+  const timesForADenominator =
+    machine.A.y * machine.B.x - machine.A.x * machine.B.y;
+  const timesForANumerator =
+    machine.Prize.y * machine.B.x - machine.Prize.x * machine.B.y;
+  const timesForA = timesForANumerator / timesForADenominator;
 
-    const neighbor = gardens[neighborRowIndex][neighborColIndex];
+  const timesForBNumerator = machine.Prize.x - machine.A.x * timesForA;
+  const timesForBDenominator = machine.B.x;
+  const timesForB = timesForBNumerator / timesForBDenominator;
 
-    positions.push([neighbor, neighborRowIndex, neighborColIndex]);
-  }
-
-  // left
-  if (colIndex > 0) {
-    const neighborRowIndex = rowIndex;
-    const neighborColIndex = colIndex - 1;
-    const neighbor = gardens[neighborRowIndex][neighborColIndex];
-
-    positions.push([neighbor, neighborRowIndex, neighborColIndex]);
-  }
-
-  // right
-  if (colIndex < gardens[0].length - 1) {
-    const neighborRowIndex = rowIndex;
-    const neighborColIndex = colIndex + 1;
-    const neighbor = gardens[neighborRowIndex][neighborColIndex];
-
-    positions.push([neighbor, neighborRowIndex, neighborColIndex]);
-  }
-
-  // bottom
-  if (rowIndex < gardens.length - 1) {
-    const neighborRowIndex = rowIndex + 1;
-    const neighborColIndex = colIndex;
-
-    const neighbor = gardens[neighborRowIndex][neighborColIndex];
-
-    positions.push([neighbor, neighborRowIndex, neighborColIndex]);
-  }
-
-  return positions;
+  return {
+    A: timesForA,
+    B: timesForB,
+  };
 };
 
-const regionArea = (region) => {
-  return region.size;
+const numberOfTokens = (timesForAandB) => {
+  return timesForAandB.A * 3 + timesForAandB.B * 1;
 };
 
-const parsedRowAndCol = (rowAndColString) => {
-  const [plotRowString, plotColString] = rowAndColString.split("|");
+const numberOfTokenToWinAllPrizes = (machines) => {
+  let totalTokens = 0;
 
-  return [Number(plotRowString), Number(plotColString)];
-};
+  machines.forEach((machine) => {
+    const times = timesForAandB(machine);
+    const tokens = numberOfTokens(times);
+    const isPossibleToWin = Number.isInteger(tokens);
 
-const adjNotSameNamePos = (row, col, gardens) => {
-  const regionName = gardens[row][col];
-  const pos = [];
-
-  if (row === 0 || gardens[row - 1][col] !== regionName) {
-    pos.push(`top|${row}|${col}`);
-  }
-
-  if (row === gardens.length - 1 || gardens[row + 1][col] !== regionName) {
-    pos.push(`bottom|${row}|${col}`);
-  }
-
-  if (col === 0 || gardens[row][col - 1] !== regionName) {
-    pos.push(`left|${row}|${col}`);
-  }
-
-  if (col === gardens[0].length - 1 || gardens[row][col + 1] !== regionName) {
-    pos.push(`right|${row}|${col}`);
-  }
-
-  return pos;
-};
-
-const regionNumberOfSides = (region, gardens) => {
-  const plots = Array.from(region);
-
-  console.log("plots", plots);
-  const outsidePos = new Set();
-
-  for (let i = 0; i < plots.length; i++) {
-    const [currentRow, currentCol] = parsedRowAndCol(plots[i]);
-    adjNotSameNamePos(currentRow, currentCol, gardens).forEach((pos) =>
-      outsidePos.add(pos),
-    );
-  }
-
-  const positions = Array.from(outsidePos);
-  const filteredPos = positions.filter((pos) => {
-    if (pos.startsWith("top")) {
-      const [direction, row, col] = pos.split("|");
-      return !positions.includes(`${direction}|${row}|${Number(col - 1)}`);
-    } else if (pos.startsWith("bottom")) {
-      const [direction, row, col] = pos.split("|");
-      return !positions.includes(`${direction}|${row}|${Number(col - 1)}`);
-    } else if (pos.startsWith("left")) {
-      const [direction, row, col] = pos.split("|");
-      return !positions.includes(
-        `${direction}|${Number(row - 1)}|${Number(col)}`,
-      );
-    } else if (pos.startsWith("right")) {
-      const [direction, row, col] = pos.split("|");
-      return !positions.includes(
-        `${direction}|${Number(row - 1)}|${Number(col)}`,
-      );
+    if (isPossibleToWin) {
+      totalTokens += tokens;
     }
-
-    return true;
   });
 
-  console.log("outsidePos", outsidePos);
-
-  console.log("filteredPos", filteredPos, filteredPos.length);
-  return filteredPos.length;
+  return totalTokens;
 };
 
-const findRegion = (initialRow, initialCol, gardens) => {
-  const updatedGardens = [...gardens];
-  const differentGardenNameToExplore = new Set();
-  let sameGardenNameToExplore = new Set([`${initialRow}|${initialCol}`]);
-  const region = new Set([`${initialRow}|${initialCol}`]);
-
-  const visited = new Set();
-
-  while (sameGardenNameToExplore.size > 0) {
-    const currentGardensSameName = Array.from(sameGardenNameToExplore);
-    const currentPlotString = currentGardensSameName.pop();
-
-    if (visited.has(currentPlotString)) {
-      continue;
-    }
-
-    const [row, col] = currentPlotString.split("|");
-    const rowIndex = Number(row);
-    const colIndex = Number(col);
-
-    adjacentPlotsWithPositions(rowIndex, colIndex, gardens).forEach(
-      ([plotName, plotRow, plotCol]) => {
-        const plotToString = `${plotRow}|${plotCol}`;
-
-        if (
-          plotName === gardens[rowIndex][colIndex] &&
-          !visited.has(plotToString)
-        ) {
-          currentGardensSameName.push(plotToString);
-          region.add(plotToString);
-        } else if (!visited.has(plotToString) && plotName !== ".") {
-          differentGardenNameToExplore.add(plotToString);
-        }
-      },
-    );
-
-    sameGardenNameToExplore = new Set(currentGardensSameName);
-    visited.add(currentPlotString);
-  }
-
-  Array.from(region).forEach((plot) => {
-    const [row, col] = plot.split("|");
-    updatedGardens[Number(row)][Number(col)] = ".";
-  });
-
-  return [region, updatedGardens, Array.from(differentGardenNameToExplore)[0]];
-};
-
-const allRegions = (gardens) => {
-  let startRow = 0;
-  let startCol = 0;
-  let currentGardens = [...gardens].map((row) => [...row]);
-  const regions = [];
-
-  let i = 0;
-
-  while (Number.isFinite(startRow) && Number.isFinite(startCol)) {
-    const [currentRegion, updatedGardens, startPosString] = findRegion(
-      startRow,
-      startCol,
-      currentGardens,
-    );
-
-    if (!startPosString) {
-      startRow = undefined;
-      startCol = undefined;
-
-      for (let row = 0; row < updatedGardens.length; row++) {
-        for (let col = 0; col < updatedGardens[0].length; col++) {
-          if (updatedGardens[row][col] !== ".") {
-            startRow = row;
-            startCol = col;
-            break;
-          }
-        }
-      }
-    } else {
-      const [rowString, colString] = startPosString.split("|");
-      startRow = Number(rowString);
-      startCol = Number(colString);
-    }
-
-    currentGardens = updatedGardens;
-    regions.push(currentRegion);
-
-    if (i == 3) {
-      //break;
-    }
-    i++;
-  }
-  return regions;
-};
-
-const totalPrice = (gardens) => {
-  const regions = allRegions(gardens);
-  let total = 0;
-
-  //console.log("regions", regions);
-
-  regions.forEach((region) => {
-    const area = regionArea(region);
-    const sides = regionNumberOfSides(region, gardens);
-
-    const firstPosition = parsedRowAndCol(Array.from(region)[0]);
-    console.log(
-      "REGION",
-      gardens[firstPosition[0]][firstPosition[1]],
-      area,
-      sides,
-    );
-    total += area * sides;
-  });
-
-  return total;
-};
-
-console.log(totalPrice(gardens));
+console.log(numberOfTokenToWinAllPrizes(machines));
